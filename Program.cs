32390327cad8d11e3e9bab2b4c88ea6d9c393d57
Leandro Carneiro <leandro.carneiro@weareplanet.com>
2024-01-17 09:37:40 +0000

    adding proj

--- Program.cs
+++ Program.cs
@@ -1,4 +1,18 @@
-public void Main(string[] args)
+﻿public static class Program
 {
-    
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("A - Perceptron");
+        Console.WriteLine("B - Perceptron Backpropagation");
+        var option = "A";//Console.ReadLine();
+
+        // BaseFlow flow;
+
+        // if (option == "A")
+        //     flow = new PerceptronFlow();
+        // else
+        //     flow = new PerceptronBackpropagationFlow();
+
+        // flow.Execute();
+    }
 }
